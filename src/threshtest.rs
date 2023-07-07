3cbf46c35102803da828a639a99e1a0875d05f6c
kmgill <apoapsys@gmail.com>
2023-07-07 13:37:17 -0700

    Added an rgb threshtest option

--- src/threshtest.rs
+++ src/threshtest.rs
@@ -1,5 +1,41 @@
 use sciimg::prelude::*;
 use sciimg::Dn;
+use itertools::iproduct;
+
+pub fn compute_rgb_threshtest_image(frame: &Image, threshold: Dn) -> Image {
+
+    let mut out = Image::new_with_bands(frame.width, frame.height, 3, frame.get_mode()).unwrap();
+
+    let max_val = if frame.get_mode() == ImageMode::U8BIT { 255.0 } else { 65535.0 };
+
+    iproduct!(0..frame.height, 0..frame.width).for_each(|(y, x)|{
+
+        let (r, g, b) = if frame.num_bands() == 1 {
+            let v = frame.get_band(0).get(x,y);
+            let t = if v >= threshold {
+                max_val
+            } else {
+                v
+            };
+            (t, v, v)
+        } else  {
+            let r = frame.get_band(0).get(x, y);
+            let g = frame.get_band(1).get(x, y);
+            let b = frame.get_band(2).get(x, y);
+            (
+                if r >= threshold { max_val} else { r },
+                if g >= threshold { max_val} else { g },
+                if b >= threshold { max_val} else { b }
+            )
+        };
+
+        out.put(x, y, r, 0);
+        out.put(x, y, g, 1);
+        out.put(x, y, b, 2);
+    });
+
+    out
+}
 
 pub fn compute_threshtest_image(frame: &Image, threshold: Dn) -> ImageBuffer {
     info!(
