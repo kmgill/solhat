ee452ec41e5bb0376aa2a7bd82376c64bcf6af24
kmgill <apoapsys@gmail.com>
2023-07-17 11:07:56 -0400

    Added a none / equatorial mount option for Target

--- src/target.rs
+++ src/target.rs
@@ -1,6 +1,8 @@
 use crate::{lunar, parallacticangle, solar, timestamp::TimeStamp};
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default)]
 pub struct TargetPosition {
     pub rotation: f64,
     pub altitude: f64,
@@ -12,13 +14,14 @@ pub enum Target {
     #[default]
     Sun,
     Moon,
-    // Mercury,
-    // Venus,
-    // Mars,
-    // Jupiter,
-    // Saturn,
-    // Uranus,
-    // Neptune,
+    None, // No rotation, Using equatorial mount
+          // Mercury,
+          // Venus,
+          // Mars,
+          // Jupiter,
+          // Saturn,
+          // Uranus,
+          // Neptune,
 }
 
 impl Target {
@@ -26,6 +29,7 @@ impl Target {
         match s.to_uppercase().as_str() {
             "MOON" => Ok(Target::Moon),
             "SUN" => Ok(Target::Sun),
+            "NONE" => Ok(Target::None),
             _ => Err(anyhow!("Invalid target supplied: '{}'", s)),
         }
     }
@@ -36,23 +40,29 @@ impl Target {
         obs_longitude: f64,
         ts: &TimeStamp,
     ) -> Result<TargetPosition> {
-        let (altitude, azimuth) = match self {
-            Target::Moon => {
-                info!("Calculating position for Moon");
-                lunar::position_from_lat_lon_and_time(obs_latitude, obs_longitude, ts)
-            }
-            Target::Sun => {
-                info!("Calculating position for Sun");
-                solar::position_from_lat_lon_and_time(obs_latitude, obs_longitude, ts)
-            }
-        };
+        if *self == Target::None {
+            return Ok(TargetPosition::default());
+        } else {
+            let (altitude, azimuth) = match self {
+                Target::Moon => {
+                    info!("Calculating position for Moon");
+                    lunar::position_from_lat_lon_and_time(obs_latitude, obs_longitude, ts)
+                }
+                Target::Sun => {
+                    info!("Calculating position for Sun");
+                    solar::position_from_lat_lon_and_time(obs_latitude, obs_longitude, ts)
+                }
+                _ => return Err(anyhow!("Unsupported target for rotation")),
+            };
 
-        let rotation = parallacticangle::from_lat_azimuth_altitude(obs_latitude, azimuth, altitude);
+            let rotation =
+                parallacticangle::from_lat_azimuth_altitude(obs_latitude, azimuth, altitude);
 
-        Ok(TargetPosition {
-            rotation,
-            altitude,
-            azimuth,
-        })
+            Ok(TargetPosition {
+                rotation,
+                altitude,
+                azimuth,
+            })
+        }
     }
 }
